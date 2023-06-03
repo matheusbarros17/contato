fd532ba517badbfccc9f5732407ed923852c9afb
matheusbarros17 <matheuscarvalho_5@hotmail.com>
2023-06-03 18:16:00 -0300

    atualização

--- src/main/java/com/example/contato/controller/ContatoREST.java
+++ src/main/java/com/example/contato/controller/ContatoREST.java
@@ -3,6 +3,7 @@ package com.example.contato.controller;
 import com.example.contato.entidade.Pessoa;
 import com.example.contato.repositorio.RepositorioContato;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,17 +17,20 @@ public class ContatoREST {
     private RepositorioContato repositorio;
 
     @GetMapping(path = "/consultar")
+    @ResponseStatus(HttpStatus.OK)
     public List<Pessoa> listarTodos() {
         return repositorio.findAll();
     }
 
     @PostMapping(path = "/salvar")
+    @ResponseStatus(HttpStatus.CREATED)
     //@RequestBody converte os dados em JSON para objeto JAVA
     public void salvar (@RequestBody Pessoa pessoa) {
         repositorio.save(pessoa);
     }
 
     @PutMapping(path = "/atualizar")
+    @ResponseStatus(HttpStatus.OK)
     //@RequestBody converte os dados em JSON para objeto JAVA
     public void alterar (@RequestBody Pessoa pessoa) {
         if (pessoa.getId() > 0) {
@@ -35,6 +39,7 @@ public class ContatoREST {
     }
 
     @DeleteMapping(path = "/excluir")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void excluir (@RequestBody Pessoa pessoa) {
         repositorio.delete(pessoa);
     }
